ebe04d20347d162bb9f7de747acc775006b34358
Oscar Caicedo <oscarccd17@gmail.com>
2023-04-25 21:59:30 -0500

    creating initial fetch call

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log(apiKey);
+import { API_KEY } from "./config.js";
+
+async function fetchData(){
+    const response = await fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${API_KEY}`
+        }
+    });
+}
