228f6d5289d9b89fa5b029cc7b2108b70c0191f8
Oscar Caicedo <oscarccd17@gmail.com>
2023-04-26 20:10:32 -0500

    api built and data object returned

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ import { API_KEY } from "./config.js";
 
 async function fetchData(){
     const response = await fetch("https://api.openai.com/v1/completions", {
-        method: "POST",
-        headers: {
-            Authorization: `Bearer ${API_KEY}`
-        }
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: "hello, how are you today?",
+        max_tokens: 7
+      })
     });
+    const data = await response.json()
+    console.log(data)
 }
+
+fetchData()
