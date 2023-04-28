f66086e0590973ebadce7fdecb9babe15d2140da
Oscar Caicedo <oscarccd17@gmail.com>
2023-04-27 19:37:19 -0500

    working through gpt-4 model and 404 error

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 import { API_KEY } from "./config.js";
 
 async function fetchData(){
-    const response = await fetch("https://api.openai.com/v1/completions", {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
         Authorization: `Bearer ${API_KEY}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "text-davinci-003",
-        prompt: "hello, how are you today?",
-        max_tokens: 7
-      })
+        model: "gpt-4",
+        messages: [{
+            role: "user",
+            content: "Hello!",
+          },],
+      }),
     });
     const data = await response.json()
     console.log(data)
