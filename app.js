b271c5e9d0beceb3212288571cfab83f3c0dd36f
Oscar Caicedo <oscarccd17@gmail.com>
2023-05-02 19:54:11 -0500

    finally fixed the comment bar

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-import { API_KEY } from "./config.js";
+// import { API_KEY } from "./config.js";
 
-async function fetchData(){
-    const response = await fetch("https://api.openai.com/v1/completions", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${API_KEY}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        model: "text-davinci-003",
-        prompt: "How are you doing today this fine evening?",
-        max_tokens: 7
-      })
-    });
-    const data = await response.json()
-    console.log(data)
-}
+// async function fetchData(){
+//     const response = await fetch("https://api.openai.com/v1/completions", {
+//       method: "POST",
+//       headers: {
+//         Authorization: `Bearer ${API_KEY}`,
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         model: "text-davinci-003",
+//         prompt: "How are you doing today this fine evening?",
+//         max_tokens: 7
+//       })
+//     });
+//     const data = await response.json()
+//     console.log(data)
+// }
 
-fetchData();
+// fetchData();
