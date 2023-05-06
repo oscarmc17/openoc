942c0cd50c33b269314c7f700096bfae2feefc58
Oscar Caicedo <oscarccd17@gmail.com>
2023-05-05 20:37:05 -0500

    request body

--- app.js
+++ app.js
@@ -24,12 +24,16 @@ const submitButton = document.querySelector('#submit')
 async function getMessage() {
   console.log('clicked')
   const options = {
-    method: 'POST',
+    method: "POST",
     headers: {
-        Authorization: `Bearer ${API_KEY}`,
-        "Content-Type": "application/json",
-      }
-  }
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: {
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: "Hello!"}]
+    },
+  };
   try {
     await fetch('https://api.openai.com/v1/completions', options)
   } catch {
