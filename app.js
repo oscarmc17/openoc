4b30eb4d11e35f8e433b0525807287f747a07e83
Oscar Caicedo <oscarccd17@gmail.com>
2023-05-07 21:15:35 -0500

    output response.json()

--- app.js
+++ app.js
@@ -29,15 +29,18 @@ async function getMessage() {
       Authorization: `Bearer ${API_KEY}`,
       "Content-Type": "application/json",
     },
-    body: {
+    body: JSON.stringify({
       model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: "Hello!"}]
-    },
+      messages: [{ role: "user", content: "Hello!" }],
+      max_tokens: 100,
+    }),
   };
   try {
-    await fetch('https://api.openai.com/v1/completions', options)
-  } catch {
-
+    const response = await fetch('https://api.openai.com/v1/completions', options)
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
   }
 }
 
